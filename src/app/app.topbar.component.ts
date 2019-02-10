7bdd42ccf04808c4d210d9bfd969f1c236d72132
haqqii <m.haqqi@difinite.com>
2019-02-10 21:12:36 +0700

    move option menu: layout, theme, custimize to topbar

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -1,14 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AppComponent} from './app.component';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-topbar',
+    styles: [``],
     template: `
         <div class="topbar clearfix">
             <div class="topbar-left">
                 <img src="assets/layout/images/logo.png" class="topbar-logo" />
             </div>
-
             <div class="topbar-right">
                 <a id="menu-button" href="#" (click)="app.onMenuButtonClick($event)"
                    [ngClass]="{'menu-button-rotate': app.rotateMenuButton}">
@@ -56,36 +59,23 @@ import {AppComponent} from './app.component';
                             </li>
                         </ul>
                     </li>
+                    <!--<ul app-topbar [item]="model" root="true" class="layout-menu fadeInDown clearfix"-->
+                        <!--[reset]="reset"></ul>-->
+                        <!--<p-tieredMenu #menu [model]="items" [popup]="true"></p-tieredMenu>-->
+                        <!--<button #btn type="button" pButton icon="pi pi-bars" label="Show" (click)="menu.toggle($event)"></button>-->
                     <li #settings [ngClass]="{'active-top-menu':app.activeTopbarItem === settings}">
                         <a href="#" (click)="app.onTopbarItemClick($event,settings)">
                             <i class="topbar-icon fa fa-fw fa-cog"></i>
                             <span class="topbar-item-name">Settings</span>
                         </a>
-                        <ul class="layout-menu fadeInDown">
-                            <li role="menuitem">
-                                <a href="#" (click)="app.onTopbarSubItemClick($event)">
-                                    <i class="fa fa-fw fa-paint-brush"></i>
-                                    <span>Change Theme</span>
-                                </a>
-                            </li>
-                            <li role="menuitem">
-                                <a href="#" (click)="app.onTopbarSubItemClick($event)">
-                                    <i class="fa fa-fw fa-star-o"></i>
-                                    <span>Favorites</span>
-                                </a>
-                            </li>
-                            <li role="menuitem">
-                                <a href="#" (click)="app.onTopbarSubItemClick($event)">
-                                    <i class="fa fa-fw fa-lock"></i>
-                                    <span>Lock Screen</span>
-                                </a>
-                            </li>
-                            <li role="menuitem">
-                                <a href="#" (click)="app.onTopbarSubItemClick($event)">
-                                    <i class="fa fa-fw fa-picture-o"></i>
-                                    <span>Wallpaper</span>
-                                </a>
-                            </li>
+                        <ul class="layout-menu fadeInDown" id="navigation">
+                            <!--<li role="menuitem">-->
+                                <!--<a href="#" (click)="app.onTopbarSubItemClick($event)">-->
+                                    <!--<i class="fa fa-fw fa-paint-brush"></i>-->
+                                    <!--<span>Change Theme</span>-->
+                                <!--</a>-->
+                            <!--</li>-->
+                                <p-tieredMenu [model]="model"></p-tieredMenu>
                         </ul>
                     </li>
                     <li #messages [ngClass]="{'active-top-menu':app.activeTopbarItem === messages}">
@@ -167,13 +157,136 @@ import {AppComponent} from './app.component';
                             <i class="fa fa-search"></i>
                         </div>
                     </li>
+                    <!--<p-tieredMenu #menu [model]="model" [popup]="true"></p-tieredMenu>-->
+                    <!--<i class="fa fa fa-fw fa-list" (click)="menu.toggle($event)"><label>Change</label></i>-->
                 </ul>
             </div>
         </div>
-    `
+    `,
 })
-export class AppTopBarComponent {
+export class AppTopBarComponent implements OnInit {
+
+    @Input() reset: boolean;
+
+    model: any[];
+
+    theme = 'blue';
+
+    layout = 'blue';
+
+    version = '-v4';
 
     constructor(public app: AppComponent) {}
 
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Customization', icon: 'fa fa-fw fa-bars' , badge: '8',
+                items: [
+                    {label: 'Static Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToStaticMenu()},
+                    {label: 'Overlay Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToOverlayMenu()},
+                    {label: 'Slim Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToSlimMenu()},
+                    {label: 'Horizontal Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToHorizontalMenu()},
+                    {label: 'Inline Profile', icon: 'fa fa-sun-o fa-fw',  command: () => this.app.profileMode = 'inline'},
+                    {label: 'Top Profile', icon: 'fa fa-moon-o fa-fw',  command: () => this.app.profileMode = 'top'},
+                    {label: 'Light Menu', icon: 'fa fa-sun-o fa-fw',  command: () => this.app.darkMenu = false},
+                    {label: 'Dark Menu', icon: 'fa fa-moon-o fa-fw',  command: () => this.app.darkMenu = true}
+                ]
+            },
+            {
+                label: 'Layout Colors', icon: 'fa fa-fw fa-magic',
+                items: [
+                            {label: 'Blue', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('blue'); }},
+                            {label: 'Purple', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('purple'); }},
+                            {label: 'Cyan', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('cyan'); }},
+                            {label: 'Indigo', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('indigo'); }},
+                            {label: 'Teal', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('teal'); }},
+                            {label: 'Pink', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('pink'); }},
+                            {label: 'Lime', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('lime'); }},
+                            {label: 'Green', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('green'); }},
+                            {label: 'Amber', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('amber'); }},
+                            {label: 'Brown', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('brown'); }},
+                            {label: 'Orange', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('orange'); }},
+                            {label: 'Deep Purple', icon: 'fa fa-fw fa-paint-brush',
+                                command: (event) => {this.changeLayout('deeppurple'); }},
+                            {label: 'Light Blue', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('lightblue'); }},
+                            {label: 'Light Green', icon: 'fa fa-fw fa-paint-brush',
+                                command: (event) => {this.changeLayout('lightgreen'); }},
+                            {label: 'Dark Grey', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('darkgrey'); }},
+                ]
+            },
+            {
+                label: 'Special', icon: 'fa fa-fw fa-paint-brush',
+                items: [
+                    {label: 'Influenza', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('influenza', true); }},
+                    {label: 'Suzy', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('suzy', true); }},
+                    {label: 'Calm', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('calm', true); }},
+                    {label: 'Crimson', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('crimson', true); }},
+                    {label: 'Night', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('night', true); }},
+                    {label: 'Skyling', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('skyline', true); }},
+                    {label: 'Sunkist', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('sunkist', true); }},
+                    {label: 'Little Leaf', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('littleleaf', true); }},
+                    {label: 'Joomla', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('joomla', true); }},
+                    {label: 'Firewatch', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeLayout('firewatch', true); }}
+                    ]
+            },
+            {
+                label: 'Themes', icon: 'fa fa-fw fa-paint-brush', badge: '5',
+                items: [
+                    {label: 'Blue', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('blue'); }},
+                    {label: 'Cyan', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('cyan'); }},
+                    {label: 'Indigo', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('indigo'); }},
+                    {label: 'Purple', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('purple'); }},
+                    {label: 'Teal', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('teal'); }},
+                    {label: 'Orange', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('orange'); }},
+                    {label: 'Deep Purple', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('deeppurple'); }},
+                    {label: 'Light Blue', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('lightblue'); }},
+                    {label: 'Green', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('green'); }},
+                    // {label: 'Light Green', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('lightgreen'); }},
+                    // {label: 'Lime', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('lime'); }},
+                    // {label: 'Amber', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('amber'); }},
+                    // {label: 'Brown', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('brown'); }},
+                    // {label: 'Dark Grey', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('darkgrey'); }},
+                    // {label: 'Pink', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('pink'); }},
+                ]
+            },
+        ];
+    }
+
+    changeTheme(theme: string) {
+        const themeLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('theme-css');
+
+        if (this.version === '-v4') {
+            themeLink.href =  'assets/theme/theme-' + theme + '-v4' + '.css';
+        }
+
+        this.theme = theme;
+
+    }
+
+    changeLayout(layout: string, special?: boolean) {
+        const layoutLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('layout-css');
+
+        if (this.version === '-v4') {
+            layoutLink.href = 'assets/layout/css/layout-' + layout + '-v4' + '.css';
+        }
+
+        this.layout = layout;
+
+        if (special) {
+            this.app.darkMenu = true;
+        }
+    }
+
+    changeVersion(version: string) {
+        const themeLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('theme-css');
+        const layoutLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('layout-css');
+
+        if (version === '-v4') {
+            themeLink.href =  'assets/theme/theme-' + this.theme + '-v4' + '.css';
+            layoutLink.href = 'assets/layout/css/layout-' + this.layout + '-v4' + '.css';
+            this.version = '-v4';
+        }
+
+    }
 }
